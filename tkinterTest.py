03ea8f4bf6fba9515c3f92b2386e757bbb1a7ae3
EdmundGoodman <egoodman3141@gmail.com>
2019-04-05 22:04:19 +0100

    Improved the GUI interface to control the robot

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,11 +1,14 @@
+#from source3 import *
+
 import tkinter as tk
 from tkinter.ttk import Radiobutton
 from tkinter import messagebox, Scale
 import atexit
 
-from source3 import *
+from time import time, sleep
+import threading
 
-"""class Robot:
+class Robot:
     def forward(self):
         pass
 
@@ -24,11 +27,14 @@ from source3 import *
     def shutdown(self):
         pass
 
-    def flyWheelsOn(self):
+    def flyWheelsOn(self, duty=""):
         pass
 
     def flyWheelsOff(self):
-        pass"""
+        pass
+
+    def getDistance(self):
+        return int(round(time(), 0))
 
 
 class NoHoverButton(tk.Button):
@@ -56,18 +62,26 @@ class App:
         self.root.title = self.TITLE
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
-        self._frame = self.getNewFrame()
+        self.frame = self.getNewFrame()
+        self.thread = None
         self.main()
-
-    def getFrame(self):
-        return self._frame
 
     def getNewFrame(self):
         #Create a Frame
-        frame = tk.Frame(self.root, width=600, height=600)
+        frame = tk.Frame(self.root, width=600, height=300)
         frame.pack(fill="both", expand=True)
         frame.grid_propagate(False)
         return frame
+
+    def videoLoop(self):
+        try:
+            while not self.stopEvent.is_set():
+                #Get the distance
+                self.distance["text"] = "Distance:\n{}".format(self.robot.getDistance())
+
+        except RuntimeError as e:
+            print("Runtime error: {}".format(e))
+
 
     def main(self):
         self.flywheelsFlag = 0
@@ -80,10 +94,9 @@ class App:
                 self.flwheels.setBackground("red")
             else:
                 self.flwheels.resetBackground()
-            print("On" if self.flywheelsFlag else "Off", self.flywheelsDuty)
+            #print("On" if self.flywheelsFlag else "Off", self.flywheelsDuty)
 
             if self.flywheelsFlag:
-                #Change source2 api to include variable flywheel duty
                 self.robot.flyWheelsOn(self.flywheelsDuty)
             else:
                 self.robot.flyWheelsOff()
@@ -94,35 +107,53 @@ class App:
         def unclicked(event=None):
             self.robot.stop()
 
-        self.up = NoHoverButton(self._frame, text="^", height=10, width=10)
-        self.up.grid(column=1, row=1)
+        self.up = NoHoverButton(self.frame, text="^", height=5, width=5)
+        self.up.grid(column=1, row=0)
         self.up.bind("<ButtonPress>", lambda x: self.robot.forward())
         self.up.bind("<ButtonRelease>", unclicked)
 
-        self.left = NoHoverButton(self._frame, text="<", height=10, width=10)
-        self.left.grid(column=0, row=2)
+        self.left = NoHoverButton(self.frame, text="<", height=5, width=5)
+        self.left.grid(column=0, row=1)
         self.left.bind("<ButtonPress>", lambda x: self.robot.turnLeft())
         self.left.bind("<ButtonRelease>", unclicked)
 
-        self.right = NoHoverButton(self._frame, text=">", height=10, width=10)
-        self.right.grid(column=2, row=2)
+        self.right = NoHoverButton(self.frame, text=">", height=5, width=5)
+        self.right.grid(column=2, row=1)
         self.right.bind("<ButtonPress>", lambda x: self.robot.turnRight())
         self.right.bind("<ButtonRelease>", unclicked)
 
-        self.down = NoHoverButton(self._frame, text="v", height=10, width=10)
-        self.down.grid(column=1, row=3)
+        self.down = NoHoverButton(self.frame, text="v", height=5, width=5)
+        self.down.grid(column=1, row=2)
         self.down.bind("<ButtonPress>", lambda x: self.robot.backward())
         self.down.bind("<ButtonRelease>", unclicked)
 
-        self.flwheels = NoHoverButton(self._frame, text="*", height=10, width=10)
-        self.flwheels.grid(column=1, row=2)
+        self.flwheels = NoHoverButton(self.frame, text="*", height=5, width=5)
+        self.flwheels.grid(column=1, row=1)
         self.flwheels.bind("<ButtonPress>", toggleFlywheels)
 
-        self.slider = Scale(self._frame, from_=1000, to=2000, tickinterval=200)
+
+
+
+        self.calibrate = NoHoverButton(self.frame, text="Calibrate\nflywheels") #, height=5, width=5)
+        self.calibrate.grid(column=3, row=1)
+        self.calibrate.bind("<ButtonPress>", lambda x: unclicked) #self.robot.flywheels.calibrate())
+        self.calibrate.bind("<ButtonRelease>", lambda x: unclicked)
+
+        self.slider = Scale(self.frame, from_=1000, to=2000, tickinterval=250, resolution=10, length=75)
         self.slider.set(self.flywheelsDuty)
         self.slider.grid(column=3, row=2)
 
+        self.distance = tk.Label(self.frame, text="Distance:\nNone")
+        self.distance.grid(column=3, row=0)
+
+        self.stopEvent = threading.Event()
+        self.thread = threading.Thread(target=self.videoLoop, args=())
+        self.thread.start()
+
+
+
     def close(self):
+        self.stopEvent.set()
         self.robot.stop()
         self.robot.flyWheelsOff()
         messagebox.showerror('Closing window!', 'You are closing the window')
