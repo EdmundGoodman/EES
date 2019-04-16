6f8eb8ba225dd8cf76b699bea56eb097059ea7d5
EdmundGoodman <egoodman3141@gmail.com>
2019-04-16 14:57:04 +0100

    Added TOF testing code

--- DiabloTest.py
+++ DiabloTest.py
@@ -1,13 +1,13 @@
-import Diablo
+import Diablo3 as Diablo
 import time
 
-print(Diablo.ScanForDiablo)
+print(Diablo.ScanForDiablo())
 
 m = Diablo.Diablo()
 m.i2cAddress = 0x25
 m.Init()
 m.SetMotors(-1)
-time.sleep(.5)
+time.sleep(.75)
 m.SetMotors(0.3)
-time.sleep(3)
+time.sleep(1)
 m.SetMotors(0)
